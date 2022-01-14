b1d4bfedcbf3d1dc43e782855f41fdea9f883e5d
Caballo <caballojay@pm.me>
2022-01-14 23:48:52 +0100

    Added update, delete and getById capabilities to salaries

--- controllers/salary-ctrl.js
+++ controllers/salary-ctrl.js
@@ -48,7 +48,81 @@ getSalaries = async (req, res) => {
     }).catch(err => console.log(err))
 }
 
+updateSalary = async (req, res) => {
+    const body = req.body
+
+    if (!body) {
+        return res.status(400).json({
+            success: false,
+            error: 'You must provide a body to update',
+        })
+    }
+
+    Salary.findOne({ _id: req.params.id }, (err, salary) => {
+        if (err) {
+            return res.status(404).json({
+                err,
+                message: 'Movie not found!',
+            })
+        }
+        salary.date = body.date
+        salary.user = body.user
+        salary.amount = body.amount
+        salary.isPayed = body.isPayed
+        salary
+            .save()
+            .then(() => {
+                return res.status(200).json({
+                    success: true,
+                    id: salary._id,
+                    message: 'Salary updated!',
+                })
+            })
+            .catch(error => {
+                return res.status(404).json({
+                    error,
+                    message: 'Salary not updated!',
+                })
+            })
+    })
+}
+
+deleteSalary = async (req, res) => {
+    await Salary.findOneAndDelete({ _id: req.params.id }, (err, salary) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!salary) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Salary not found` })
+        }
+
+        return res.status(200).json({ success: true, data: salary })
+    }).catch(err => console.log(err))
+}
+
+getSalaryById = async (req, res) => {
+    await Salary.findOne({ _id: req.params.id }, (err, salary) => {
+        if (err) {
+            return res.status(400).json({ success: false, error: err })
+        }
+
+        if (!salary) {
+            return res
+                .status(404)
+                .json({ success: false, error: `Salary not found` })
+        }
+        return res.status(200).json({ success: true, data: salary })
+    }).catch(err => console.log(err))
+}
+
+
 module.exports = {
     createSalary,
     getSalaries,
+    updateSalary,
+    deleteSalary,
+    getSalaryById
 }
--- routes/salary-router.js
+++ routes/salary-router.js
@@ -5,6 +5,9 @@ const router = express.Router()
 const salaryCtrl = require('../controllers/salary-ctrl')
 
 router.get('/salaries', salaryCtrl.getSalaries)
-router.post('/salary', salaryCtrl.createSalary)
+router.post('/new-salary', salaryCtrl.createSalary)
+router.put('/update-salary/:id', salaryCtrl.updateSalary)
+router.delete('/delete-salary/:id', salaryCtrl.deleteSalary)
+router.get('/get-salary/:id', salaryCtrl.getSalaryById)
 
 module.exports = router
