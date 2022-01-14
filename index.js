5f6ee1739334349edf0f639cb57e6eae968b8481
Caballo <caballojay@pm.me>
2022-01-14 19:44:36 +0100

    Changed port to env

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const db = require('./db')
 const salaryRouter = require('./routes/salary-router')
 
 const app = express()
-const apiPort = 3000
+const apiPort = '3000';
+
+app.set('port', apiPort);
 
 app.use(cors())
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+app.listen(process.env.PORT || apiPort, () => console.log(`Server running on port ${apiPort}`))
 
 app.use('/api', salaryRouter)
 
-module.exports = app
