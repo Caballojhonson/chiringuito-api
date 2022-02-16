c47848c8675e5f250f0b88caea49e29f1791db9a
Caballo <caballojay@pm.me>
2022-02-16 17:45:54 +0100

    Added generateOn to fixedexpense model

--- models/Fixed-model.js
+++ models/Fixed-model.js
@@ -5,6 +5,7 @@ const Fixed = new mongoose.Schema({
     createdBy: String,
     createdOn: Date,
     concept: { type: String, default: 'Gasto fijo' },
+    generateOn: {type: Number, default: 1}, //Day of month
     dueBy: {type: Number}, //Day of month
     frequency: {type: Number, required: true}
 })
