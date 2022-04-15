2fb33ff512ea2dc4012a5a3d107b6d444eee2753
caballo <imanol.l.p@hotmail.com>
2022-04-15 21:36:41 +0200

    Quick cleanup

--- cron-jobs/debtGeneration.js
+++ cron-jobs/debtGeneration.js
@@ -1,45 +1,39 @@
-const cron = require('node-cron')
-const FixedModel = require('../models/Fixed-model')
-const DebtModel = require('../models/Debt-model')
-const ItemModel = require('../models/Item-model')
+const cron = require('node-cron');
+const FixedModel = require('../models/Fixed-model');
+const DebtModel = require('../models/Debt-model');
 
 async function getFixedExpenses() {
-    FixedModel
-    .find({})
-    .then(doc => scheduleDebts(doc))
-    .catch(err => console.log(err))
+	FixedModel.find({})
+		.then((doc) => scheduleDebts(doc))
+		.catch((err) => console.log(err));
 }
 
 function scheduleDebts(fixedExpenses) {
-    fixedExpenses.forEach(fixedExpense => {
-        return cron.schedule(`1 0 1 * *`, async () => {
-            await generateDebtFrom(fixedExpense)
-        })
-    })
+	fixedExpenses.forEach((fixedExpense) => {
+		return cron.schedule(`1 0 1 * *`, async () => {
+			await generateDebtFrom(fixedExpense);
+		});
+	});
 }
 
 function generateDebtFrom(fixedExpense) {
-    let newDebt = new DebtModel({
-        amount: fixedExpense.amount,
-        generatedBy: 'node-cron',
-        generatedOn: new Date(),
-        isRecurrent: true,
-        concept: fixedExpense.concept,
-        dueBy: fixedExpense.dueBy
-    })
+	let newDebt = new DebtModel({
+		amount: fixedExpense.amount,
+		generatedBy: 'node-cron',
+		generatedOn: new Date(),
+		isRecurrent: true,
+		concept: fixedExpense.concept,
+		dueBy: fixedExpense.dueBy,
+	});
 
-    newDebt.save()
-        .then(doc => console.log(doc))
-        .catch(err => console.error(err))
-}   
-
-
-function generateDebts() {
-    getFixedExpenses()
+	newDebt
+		.save()
+		.then((doc) => console.log(doc))
+		.catch((err) => console.error(err));
 }
 
-module.exports = generateDebts
+function generateDebts() {
+	getFixedExpenses();
+}
 
-// Get fixedExpenses
-// Generate debts through cronjob
-// 
+module.exports = generateDebts;
